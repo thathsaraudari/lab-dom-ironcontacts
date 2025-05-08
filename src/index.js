5d32eb5ec9d5ef4ee36b7a9a9681a11fdcb00413
thathsaraudari <105314224+thathsaraudari@users.noreply.github.com>
2025-05-08 20:06:21 +0200

    completed Lab Dom

--- src/index.js
+++ src/index.js
@@ -37,25 +37,80 @@ tableBody.appendChild(exampleRow);
 // ITERATION 1 - Display 3 contacts
 // Get the first 3 contacts from the 'contacts' array.
 const threeContacts = contacts.splice(0, 3);
+console.log(threeContacts);
 
-// Your code goes here ...
+for(let i = 0; i < threeContacts.length; i++) {
+  let contact = threeContacts[i];
+
+  const row = document.createElement("tr");
+  row.innerHTML = `
+    <td>
+      <img src="${contact.pictureUrl}" />
+    </td>
+    <td> ${contact.name} </td>
+    <td> ${contact.popularity.toFixed(2)} </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+  `;
 
 
-  
+  tableBody.appendChild(row);
+
+  let deleteButton = row.querySelector(".btn-delete");
+
+  deleteButton.addEventListener("click", function(e) {
+    row.remove();
+  });
+
+
+  let likeButton = row.querySelector(".btn-like");
+
+  likeButton.addEventListener("click", function(e) {
+    likeButton.classList.toggle("selected");
+  });
+
+
+  tableBody.appendChild(row);
+}
   // ITERATION 2 - Delete Buttons
   
   // Your code goes here ...
   
-  
-
   // ITERATION 3 - Like Buttons
 
   // Your code goes here ...
 
-  
-  
-
-
 // Bonus: ITERATION 4 - Add Random Contacts
 
-// Your code goes here ...
+buttonAddRandom.addEventListener("click", function() {
+
+  const randomIndex = Math.floor(Math.random() * contacts.length);
+  const splicedArr = contacts.splice(randomIndex, 1);
+  const randomContact = splicedArr[0];
+
+  const addRandomContact = document.createElement("tr");
+  addRandomContact.innerHTML = `
+  <td>
+    <img src="${randomContact.pictureUrl}" />
+  </td>
+  <td> ${randomContact.name} </td>
+  <td> ${randomContact.popularity.toFixed(2)} </td>
+  <td>
+    <button class="btn-delete">Delete</button>
+  </td>
+  <td>
+    <button class="btn-like">
+      <img src="./images/icon.png" alt="like" />
+    </button>
+  </td>
+`;
+
+tableBody.appendChild(addRandomContact);
+
+});
